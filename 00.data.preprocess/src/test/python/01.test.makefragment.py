0dcea0686195a1352e738dc147189af5102dc2fc
beyondpie <szu@health.ucsd.edu>
2024-06-11 10:17:36 -0700

    minor: update 01.test.makeframgment script

--- 00.data.preprocess/src/test/python/01.test.makefragment.py
+++ 00.data.preprocess/src/test/python/01.test.makefragment.py
@@ -6,6 +6,8 @@ import pyprojroot
 proj_root = pyprojroot.here()
 raw_bam_dir: str = os.path.join(proj_root, "data",
                                 "raw_bam_test")
+dedup_bam_dir: str = os.path.join(proj_root, "data",
+                                  "r")
 samples: List[str] = [
     "CEMBA181023_6B", "CEMBA201210_10D"]
 outdir = os.path.join(proj_root, "00.data.preprocess",
@@ -15,7 +17,7 @@ for s in samples:
     stat = sa2.pp.make_fragment_file(
         bam_file = os.path.join(raw_bam_dir, f"{s}.bam"),
         output_file = os.path.join(outdir,
-                                          f"{s}.frag.rawbam.tsv"),
+                                   f"{s}.frag.rawbam.tsv"),
         is_paired = True,
         barcode_regex = "^(\w+):.+",
         shift_left = 4,
@@ -24,5 +26,17 @@ for s in samples:
     )
     print(stat)
 
-
+for s in samples:
+    stat = sa2.pp.make_fragment_file(
+        bam_file = os.path.join(proj_root, "data",
+                                "filtered_dedup_bam",
+                                f"{s}.filtered_dedup.bam"),
+        output_file = os.path.join(outdir,
+                                   f"{s}.frag.filtered_debup_bam.tsv"),
+        is_paired = True,
+        barcode_regex = "^(\w+):.+",
+        shift_left = 4,
+        shift_right = -4,
+        min_mapq = 30
+    )
 
