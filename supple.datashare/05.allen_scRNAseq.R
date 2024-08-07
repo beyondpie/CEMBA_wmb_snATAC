8b8482be45150747351888a2be04032e4e37f7c6
beyondpie <szu@health.ucsd.edu>
2024-08-07 14:14:21 -0700

    minor

--- supple.datashare/05.allen_scRNAseq.R
+++ supple.datashare/05.allen_scRNAseq.R
@@ -1,2 +1,10 @@
 library(tidyverse)
 library(Seurat)
+
+# meta
+projd <- "/projects/ps-renlab2/szu/projects/CEMBA2"
+allend <- file.path(projd, "19.snap2_integration", "out",
+  "transferLabel_tscc")
+
+# check Allen dataset
+r <- readRDS(file.path(allend, "allen_ds1000_seurat.rds"))
