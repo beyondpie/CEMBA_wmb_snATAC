2f7abb470a049c4aab8cda75719b168e60c29cf4
beyondpie <szu@health.ucsd.edu>
2024-08-07 14:15:09 -0700

    minro

--- supple.datashare/05.allen_scRNAseq.R
+++ supple.datashare/05.allen_scRNAseq.R
@@ -7,4 +7,6 @@ allend <- file.path(projd, "19.snap2_integration", "out",
   "transferLabel_tscc")
 
 # check Allen dataset
+# this is all platform(10xv3, 10xv3, snRNA); male only;
+# downsampled on subclass level (latest subclass)
 r <- readRDS(file.path(allend, "allen_ds1000_seurat.rds"))
